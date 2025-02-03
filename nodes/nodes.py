694b7a6ddcb8940ee89dc1a84cc5758e03da9734
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-03 19:23:39 +0100

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -12,6 +12,8 @@ import numpy as np
 import cv2
 import torch
 from PIL import Image
+from typing import Any
+from server import PromptServer
 
 # Tensor to PIL function.
 def tensor2pil(image):
@@ -25,13 +27,13 @@ def pil2tensor(image):
     # Return tensor.
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
-#class AnyType(str):
-#    '''A special class that is always equal in not equal comparisons. Credit to Rgthree / pythongosssss'''
-#
-#    def __ne__(self, __value: object) -> bool:
-#        return False
-#
-#any = AnyType("*")
+class AnyType(str):
+    '''A special class that is always equal in not equal comparisons.'''
+    
+    def __ne__(self, __value: object) -> bool:
+        return False
+
+anyType = AnyType("*")
 
 class CircleDetection:
     '''Circle detection node.'''
@@ -54,14 +56,18 @@ class CircleDetection:
         }
 
     #RETURN_TYPES = ("IMAGE", "MASK", "STRING",)
-    RETURN_TYPES = ("IMAGE", "STRING",)
-    #RETURN_NAMES = ("IMAGE", "MASK, "TEXT",)
+    RETURN_TYPES = ("IMAGE", "IMAGE", "STRING",)
+    RETURN_NAMES = ("image_out", "image_mask", "data",)
+    #RETURN_NAMES = ("IMG0", "IMG1, "DATA",)
     FUNCTION = "circle_detection"
     CATEGORY = "🧬 Object Detection Nodes"
     OUTPUT_NODE = True
 
     def draw_circles(self, img, detected_circles, debug, color_tuple_str, thickness):
         '''Draw circles.'''
+        height, width, channels = img.shape
+        print(height, width, channels)
+        blank_image = np.zeros((height,width,3), np.uint8)
         outstr = ""
         print(color_tuple_str)
         # Copy image to a new image.
@@ -89,6 +95,11 @@ class CircleDetection:
                 a, b, r = pnt[0], pnt[1], pnt[2]
                 # Draw the circumference of the circle.
                 cv2.circle(newImg, (a, b), r, color_tuple, thickness)
+                cv2.circle(blank_image, (a, b), r, (255,255,255), -1)
+                #gray = cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY)
+                #ret,thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+                #_,contours,_ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                #blank_image = cv2.drawContours(blank_image, contours, -1, (255,255,255), thickness=-1)
                 # Draw a small circle of radius 1 to show the center.
                 cv2.circle(newImg, (a, b), 1, color_tuple, 3)
                 # Print dimensions and radius.
@@ -96,7 +107,7 @@ class CircleDetection:
                     print("No.:", count, "x:", a, "y", b, "r:", r)
                     outstr = outstr + "No. " + str(count) + " x: " + str(a) + " y: " + str(b) + " r: " + str(r) + "\n"
         # Return image, co-ordinates and radius.
-        return newImg, (a, b, r), outstr
+        return newImg, (a, b, r), outstr, blank_image
 
     def pre_img(self, img):
         '''Preprocess image.'''
@@ -134,9 +145,9 @@ class CircleDetection:
     def post_img(self, img, detected_circles, debug, color_tuple, thickness):
         '''Postprocess image.'''
         # Draw circles.
-        img, (a, b, r), outstr = self.draw_circles(img, detected_circles, debug, color_tuple, thickness)
+        img, (a, b, r), outstr, blank_image = self.draw_circles(img, detected_circles, debug, color_tuple, thickness)
         # Return image and tuple.
-        return img, (a, b, r), outstr
+        return img, (a, b, r), outstr, blank_image
 
     def circle_detection(self, image, threshold_canny_edge, threshold_circle_center, minR, maxR, minDist, dp, color_tuple, thickness):
         '''Main script function.'''
@@ -158,13 +169,65 @@ class CircleDetection:
         # Process image. Detect circles.
         detected_circles = self.detect_circles(gray_blur, threshold_canny_edge, threshold_circle_center, minR, maxR, minDist, dp, debug)
         # Postrocess image.
-        img_output, _, out_string = self.post_img(img_input, detected_circles, debug, color_tuple, thickness)
+        img_output, _, out_string, blank_image = self.post_img(img_input, detected_circles, debug, color_tuple, thickness)
         # Create output image.
         img_output = Image.fromarray(img_output)
         # Create tensor.
         image_out = pil2tensor(img_output)
-        # Create simple mask for testing purposes.
-        #out_mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+        blank_image = pil2tensor(blank_image)
         # Return None.
         #return (image_out, out_mask, out_string,)
-        return (image_out, out_string,)
+        return (image_out, blank_image, out_string,)
+
+def updateDataWidget(node, widget, text):
+    """
+    Raises an event to update a widget's text.
+    """
+    # It is my understanding that this is supposed to work via the
+    # "ui" return value, but that appears to no longer be the case
+    # in the latest version of ComfyUI.
+    PromptServer.instance.send_sync("exectails.text_updater.node_processed", {"node": node, "widget": widget, "text": text})
+
+class ShowData:
+    '''A node that takes any value and displays it as a string.
+    '''
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "input": (anyType, {"forceInput": True}),
+                "data": ("STRING", {"default": "", "multiline": True}),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+            },
+        }
+
+    RETURN_TYPES = ()
+    INPUT_IS_LIST = (True,)
+    OUTPUT_NODE = True
+
+    CATEGORY = "🧬 Object Detection Nodes"
+    FUNCTION = "process_data"
+
+    def process_data(self, input, data, unique_id):
+        displayText = self.render_data(input)
+        updateDataWidget(unique_id, "data", displayText)
+        return {"ui": {"data": displayText}}
+
+    def render_data(self, input):
+        '''Render data.'''
+        output = ""
+        listlen = len(input)
+        if not isinstance(input, list):
+            output = str(input)
+        elif listlen == 0:
+            output = ""
+        elif listlen == 1:
+            output = str(input[0])
+        else:
+            for i, element in enumerate(input):
+                output += f"{str(input[i])}\n"
+            output = output.strip()    
+        return output
