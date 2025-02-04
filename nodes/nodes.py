17abaf5b464f188039b5f8831afbfb8748f6d511
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-04 16:14:37 +0100

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -6,6 +6,7 @@
 # pylint: disable=too-many-positional-arguments
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-locals
+# pylint: disable=bare-except
 
 # Import the Python modules.
 import numpy as np
@@ -44,14 +45,14 @@ class CircleDetection:
                 "color_tuple_bg": ("STRING", {"multiline": False, "default": "(255, 0, 0)"}),
                 "color_tuple_fg": ("STRING", {"multiline": False, "default": "(0, 0, 255)"}),
                 "thickness": ("INT", {"default": 2, "min": 0, "max": 256}),
-                "exclude": ("STRING", {"multiline": False, "default": ""}),
+                "exclude_circles": ("STRING", {"multiline": False, "default": ""}),
             }
         }
 
     RETURN_TYPES = ("IMAGE", "IMAGE", "MASK", "STRING", "STRING")
     RETURN_NAMES = ("image_output", "image_mask", "mask", "data", "help")
     FUNCTION = "circle_detection"
-    CATEGORY = "🧬 Object Detection Nodes"
+    CATEGORY = "🧬 Circle Detection Nodes"
     OUTPUT_NODE = True
 
     def draw_circles(self, img, detected_circles, debug, color_tuple_str, color_tuple_bg, color_tuple_fg, thickness, exlist):
@@ -91,21 +92,20 @@ class CircleDetection:
             count = 0
             for pnt in detected_circles[0, :]:
                 count += 1
-                #if not exlist:
                 if count not in exlist:
-                  # Get the circle data.
-                  a, b, r = pnt[0], pnt[1], pnt[2]
-                  # Draw the circumference of the circle.
-                  cv2.circle(newImg, (a, b), r, color_tuple, thickness)
-                  cv2.putText(newImg, str(count), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 2, 255, 4)
-                  cv2.circle(blank_image, (a, b), r, color_tuple_fg, -1)
-                  cv2.circle(maskImage, (a, b), r, (255,255,255), -1)
-                  # Draw a small circle of radius 1 to show the center.
-                  cv2.circle(newImg, (a, b), 1, color_tuple, 3)
-                  # Print dimensions and radius.
-                  if debug:
-                    print("No.:", count, "x:", a, "y", b, "r:", r)
-                    outstr = outstr + "No. " + str(count) + " x: " + str(a) + " y: " + str(b) + " r: " + str(r) + "\n"
+                    # Get the circle data.
+                    a, b, r = pnt[0], pnt[1], pnt[2]
+                    # Draw the circumference of the circle.
+                    cv2.circle(newImg, (a, b), r, color_tuple, thickness)
+                    cv2.putText(newImg, str(count), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 2, 255, 4)
+                    cv2.circle(blank_image, (a, b), r, color_tuple_fg, -1)
+                    cv2.circle(maskImage, (a, b), r, (255,255,255), -1)
+                    # Draw a small circle of radius 1 to show the center.
+                    cv2.circle(newImg, (a, b), 1, color_tuple, 3)
+                    # Print dimensions and radius.
+                    if debug:
+                        print("No.:", count, "x:", a, "y", b, "r:", r)
+                        outstr = outstr + "No. " + str(count) + " x: " + str(a) + " y: " + str(b) + " r: " + str(r) + "\n"
         # Return image, co-ordinates and radius.
         return newImg, (a, b, r), outstr, blank_image, maskImage
 
@@ -140,19 +140,15 @@ class CircleDetection:
         # Return image and tuple.
         return img, (a, b, r), outstr, blank_image, maskImage
 
-    def circle_detection(self, image, threshold_canny_edge, threshold_circle_center, minR, maxR, minDist, dp, color_tuple_circle, color_tuple_bg, color_tuple_fg, thickness, exclude):
+    def circle_detection(self, image, threshold_canny_edge, threshold_circle_center, minR, maxR, minDist, dp,
+                         color_tuple_circle, color_tuple_bg, color_tuple_fg, thickness, exclude_circles):
         '''Main script function.'''
         if exclude != "":
-            #print(type(exclude))
             inlist = exclude.split(",")
-            #print(inlist)
-            #print(type(inlist))
             exlist = list(map(str.strip, inlist))
-            #print(exlist)
             exlist = list(map(int, exlist))
-            #print(exlist)
         else:
-          exlist = []
+            exlist = []
         # Print detection parameters.
         print("Threshold canny edge:", threshold_canny_edge)
         print("Threshold circle center:", threshold_circle_center)
